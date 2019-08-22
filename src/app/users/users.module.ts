d5529ce394f0362f061966a22eb8665466b18933
Adriano Avelino <adriano-st777@hotmail.com>
2019-08-21 21:57:09 -0300

    adds authorization in routes

---
+++ src/app/users/users.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { FormsModule } from '@angular/forms';
+// import { HttpModule } from '@angular/http';
+import { MaterializeModule } from 'ng2-materialize';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+@NgModule({
+  declarations: [LoginComponent, RegisterComponent],
+  imports: [
+    CommonModule,
+    FormsModule,
+    // HttpModule,
+    BrowserAnimationsModule,
+    MaterializeModule.forRoot()
+  ],
+  exports: [
+    LoginComponent,
+    RegisterComponent,
+  ]
+})
+export class UsersModule { }
